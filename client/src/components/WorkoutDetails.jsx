3cae58ea423002d9c4d672f2103e9012e340f34a
jeddeveloper <opaonjade@gmail.com>
2024-02-06 16:17:29 +0800

    Fucked up shit. Added Some Books component feature and model but everything went to shit

--- client/src/components/WorkoutDetails.jsx
+++ client/src/components/WorkoutDetails.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import WorkoutModal from "./home/WorkoutModal";
 
 // date fns
 import formatDistancetoNow from "date-fns/formatDistanceToNow";
 
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleEdit = () => {
+    console.log("edit");
+  };
+
   const handleClick = async () => {
     const response = await fetch(
       "http://localhost:3000/api/workouts/" + workout._id,
@@ -20,23 +28,41 @@ const WorkoutDetails = ({ workout }) => {
   };
 
   return (
-    <div className="workout-details">
-      <h4>{workout.title}</h4>
-      <p>
-        <strong>Load (kg): </strong>
-        {workout.load}
-      </p>
-      <p>
-        <strong>Number of reps: </strong>
-        {workout.reps}
-      </p>
-      <p>
-        {" "}
-        {formatDistancetoNow(new Date(workout.createdAt), { addSuffix: true })}
-      </p>
-      <span className="material-symbols-outlined" onClick={handleClick}>
-        Delete
-      </span>
+    <div>
+      <div className="workout-details">
+        <h4>{workout.title}</h4>
+        <p>
+          <strong>Load (kg): </strong>
+          {workout.load}
+        </p>
+        <p>
+          <strong>Number of reps: </strong>
+          {workout.reps}
+        </p>
+        <p>
+          {" "}
+          {formatDistancetoNow(new Date(workout.createdAt), {
+            addSuffix: true,
+          })}
+        </p>
+        <span
+          className="material-symbols-outlined delete"
+          onClick={handleClick}
+        >
+          Delete
+        </span>
+        <span
+          className="material-symbols-outlined edit"
+          onClick={() => setShowModal(true)}
+        >
+          Edit
+        </span>
+      </div>
+      <div>
+        {showModal && (
+          <WorkoutModal workout={workout} onClose={() => setShowModal(false)} />
+        )}
+      </div>
     </div>
   );
 };
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout Buddy</h1>
+          <h1>Listed Workouts</h1>
         </Link>
       </div>
     </header>
